1a7d524bd81e89042118d4245872bf21ce328a52
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-03-16 17:36:17 +0100

    rc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     keywords=['socat', 'systemd', 'utility'],
-    version="0.17",
+    version="0.18",
     install_requires=['satella'],
     package_data={'socatlord': ['systemd/socatlord.service']},
     packages=[
